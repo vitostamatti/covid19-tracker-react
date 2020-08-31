a50a0e6068d595b86cb7e2fa296ace7c31a88aa2
Vito Stamatti <vitostamatti@Vitos-MacBook-Pro.local>
2020-08-31 16:18:58 -0300

    Updates

---
+++ precache-manifest.3914ef2460775a680e9db50fc270474a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3acc06c571993bd604b3c0d49726c7",
+    "url": "/covid19-tracker-react/index.html"
+  },
+  {
+    "revision": "dcaed4a033dcf5638cfd",
+    "url": "/covid19-tracker-react/static/css/main.16e0888c.chunk.css"
+  },
+  {
+    "revision": "5f337a40670cd6b5a2a1",
+    "url": "/covid19-tracker-react/static/js/2.f12745e8.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19-tracker-react/static/js/2.f12745e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcaed4a033dcf5638cfd",
+    "url": "/covid19-tracker-react/static/js/main.c584392a.chunk.js"
+  },
+  {
+    "revision": "0a910a4c557a7d509010",
+    "url": "/covid19-tracker-react/static/js/runtime-main.a9598d6c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker-react/static/media/image.d7265326.png"
+  }
+]);
